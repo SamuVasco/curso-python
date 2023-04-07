172e33f221b699db7ad851c812dc359fd71743b3
Samuel Vasco <127765003+SamuVasco@users.noreply.github.com>
2023-04-07 16:30:41 -0300

    update ex001, ex002 and create ex003

--- exercicios/ex001.py
+++ exercicios/ex001.py
@@ -1,1 +1,2 @@
+# Escreva um programa que mostre "Olá, Mundo!" na tela.
 print('Olá, Mundo!')
--- exercicios/ex002.py
+++ exercicios/ex002.py
@@ -1,2 +1,3 @@
+# Crie um prorgama que leia o nome de uma pessoa e mostre uma mensagem de boas vindas.
 nome = (input('Digite o seu nome: '))
 print('É um prazer te conhecer, {}'.format(nome))
