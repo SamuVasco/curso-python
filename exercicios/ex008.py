b85551765e8f399065a4395db106c62f9e9835ae
Samuel Vasco <127765003+SamuVasco@users.noreply.github.com>
2024-01-08 10:18:15 -0300

    ex do 06 até o 011

---
+++ exercicios/ex008.py
@@ -0,0 +1,8 @@
+dist = float(input('Uma distância em metros: '))
+print(f'A medida de {dist}m corresponde a: ')
+print(f'{dist/1000}Km')
+print(f'{dist/100}hm')
+print(f'{dist/10}dam')
+print(f'{dist*10}dm')
+print(f'{dist*100}cm')
+print(f'{dist*1000}mm')
